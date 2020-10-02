402d2fd0c2e03dd7866e6e636486edfce9095d4c
Khodidas_with_love_KD <41850210+Khodidas-withLoveKD@users.noreply.github.com>
2020-10-02 12:20:34 +0530

    Let's continue ;)

---
+++ FPS_Game_using_AI_v2/Assets/Scripts/Player's Script/MouseLook.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    [SerializeField]
+    private Transform playerRoot, LookRoot;
+
+    [SerializeField]
+    private bool invert;
+
+    [SerializeField]
+    private bool can_Unlock = true;
+
+    [SerializeField]
+    private float sensitivity = 5f;
+
+    [SerializeField]
+    private int smooth_Steps = 10;
+
+    [SerializeField]
+    private float smooth_Weight = 0.4f;
+
+    [SerializeField]
+    private float roll_Angle = 10f;
+
+    [SerializeField]
+    private float roll_Speed = 3f;
+
+    [SerializeField]
+    private Vector2 default_Look_Limits = new Vector2(-70f , 80f);
+
+    private Vector2 look_Angles;
+
+    private Vector2 current_Mouse_Look;
+
+    private Vector2 smooth_Move;
+
+    private float current_Roll_Angle;
+
+    private int last_Look_Frame;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnityEngine.Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LockAndUnlockCursor();
+        if (UnityEngine.Cursor.lockState == CursorLockMode.Locked)
+        {
+            LookAround();
+        }
+    }
+
+    void LockAndUnlockCursor()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(UnityEngine.Cursor.lockState == CursorLockMode.Locked)
+            {
+                UnityEngine.Cursor.lockState = CursorLockMode.None;
+            }
+            else
+            {
+                UnityEngine.Cursor.lockState = CursorLockMode.Locked;
+                UnityEngine.Cursor.visible = false;
+            }
+        }
+    }
+
+    void LookAround()
+    {
+        current_Mouse_Look = new Vector2(Input.GetAxis(MouseAxis.MOUSE_Y), Input.GetAxis(MouseAxis.MOUSE_X));
+
+        look_Angles.x += current_Mouse_Look.x * sensitivity * (invert ? 1f : -1f);
+        look_Angles.y += current_Mouse_Look.y * sensitivity;
+
+        look_Angles.x = Mathf.Clamp(look_Angles.x, default_Look_Limits.x, default_Look_Limits.y);
+
+        //rotation for z axis (little dizzy rotation) (used if player gets drunk)
+        //current_Roll_Angle = Mathf.Lerp(current_Roll_Angle, Input.GetAxisRaw(MouseAxis.MOUSE_X) * roll_Angle, Time.deltaTime * roll_Speed);
+
+        LookRoot.localRotation = Quaternion.Euler(look_Angles.x, 0f, 0f /*current_Roll_Angle*/);
+        playerRoot.localRotation = Quaternion.Euler(0f, look_Angles.y, 0f);
+ 
+    }
+}
